0562956734d3e20966f33a20e1bc9db8efac8820
virlie paglinawan <virliejoyp@yahoo.com>
2018-07-02 19:42:54 -0700

    change the time input and add id so it will be easier to manipulate in css and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,5 +16,47 @@ $(document).ready(function() {
    $(".movie-time").text(inputtedMovieTime);
    $("ul#price").append("$" + outputPrice);
  });
-
 });
+
+//business logic
+function Ticket(title, time, age) {
+  this.title = title;
+  this.time = time;
+  this.age = age;
+  this.price = 10;
+}
+
+
+// Discount Prototypes
+Ticket.prototype.timeDiscount = function() {
+  if (this.time === "Early Bird (before Noon)") {
+    this.price -= 3;
+  } else if (this.time === "Matinee (Noon-5pm)") {
+    this.price -= 3;
+  } else {
+    this.price = 10;
+  }
+  return + this.price;
+};
+
+Ticket.prototype.ageDiscount = function() {
+  if (this.age <= 12) {
+    this.price -= 3;
+  } else if (this.age >= 65) {
+    this.price -= 5;
+  } else {
+    this.price = 10;
+  }
+  return this.price;
+};
+
+Ticket.prototype.movieDiscount = function() {
+  if (this.title === "") {
+    this.price -= 6;
+  } else if (this.title === "Avengers Infinity War") {
+    this.price += 2;
+  } else {
+    this.price = 10;
+  }
+  return this.price;
+};
