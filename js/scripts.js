69de56cc41afce1d1864afbd211413f14efdad53
virlie paglinawan <virliejoyp@yahoo.com>
2018-07-02 19:17:16 -0700

    add codes in html that does not work at the moment

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-
+// user interface logic
+$(document).ready(function() {
+  $("#ticket-price").submit(function(event){
+    event.preventDefault();
+    var inputtedMovieTitle = $("#movie-title").val();
+    var inputtedMovieTime = $("#movie-time").val();
+    var inputtedAge = parseInt($("#age").val());
+    var ticketPrice = new Ticket(inputtedMovieTitle, inputtedMovieTime, inputtedAge);
